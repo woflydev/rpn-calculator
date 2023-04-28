057215c3b71f7fb5fbfeeac2048eebbd85319f07
woflydev <wolfy.coding@gmail.com>
2023-04-28 14:17:05 +1000

    error msg remove

--- frontend/script.js
+++ frontend/script.js
@@ -7,19 +7,14 @@ new Vue({
     valid: true,
     validOperators: ['*', 'x', '/', '-', '+'] },
 
-
   methods: {
     compute: function () {
       function randomErrorMessage() {
         const errorArray = [
-          "L bozo ur formula is wrong lol.",
-          "ur formula is wrong lol.",
-          "learn rpn before u try this again.",
-          "the formula's wrong, learn rpn bro."
+          "check console for error logs.",
         ]
         return errorArray[Math.floor(Math.random(0, errorArray.length) * errorArray.length)];
       }
-
 
       function reversePolish(newExpr) {
         let expr = newExpr.split(" ");
@@ -37,33 +32,41 @@ new Vue({
           else {
             let a = stack.pop();
             let b = stack.pop();
-            if(expr[i] === "+") {
+
+            if (expr[i] === "+") {
+              console.log("Adding " + a + " and " + b + ".");
               stack.push(parseInt(a) + parseInt(b));
-            } else if(expr[i] === "-") {
-                stack.push(parseInt(b) - parseInt(a));
-              } else if(expr[i] === "*") {
-                  stack.push(parseInt(a) * parseInt(b));
-              } else if(expr[i] === "/") {
-                  stack.push(parseInt(b) / parseInt(a));
-              } else if(expr[i] === "^") {
-                  stack.push(Math.pow(parseInt(b), parseInt(a)));
-              }
+            } 
+            else if (expr[i] === "-") {
+              stack.push(parseInt(b) - parseInt(a));
+            } 
+            else if (expr[i] === "*") {
+              stack.push(parseInt(a) * parseInt(b));
+            } 
+            else if (expr[i] === "/") {
+              stack.push(parseInt(b) / parseInt(a));
+            }
+            else if (expr[i] === "^") {
+              stack.push(Math.pow(parseInt(b), parseInt(a)));
+            }
           }
         }
       
         if (stack.length > 1) {
-          return "ERROR";
+          return "error";
         } 
         
         else {
-          console.log(stack[0])
           return stack[0];
         }
       }
       
+      // resets the stack to prevent spam errors
       this.stack = [];
 
-      this.stack.push(reversePolish(this.formula));
+      var result = reversePolish(this.formula);
+      console.log("Calculation Result: " + result);
+      this.stack.push(result);
 
       if (this.stack.length != 1 || isNaN(this.stack[0])) {
         this.stack = [];
@@ -73,58 +76,4 @@ new Vue({
       else {
         this.valid = true;
       }
-
-      /*for (var i = 0; i < this.formula.length; i++) {
-        var char = this.formula.substring(i, i + 1);
-
-        if (!isNaN(char) && char != " ") {
-          // pushes the character
-          this.stack.push(+char);
-          console.log("Adding new number to stack! The stack is now: ", this.stack);
-        } else if (char === " ") {
-          console.log("Continuing...")
-          continue;
-        } else {
-
-          var num1 = this.stack[this.stack.length - 2];
-          var num2 = this.stack[this.stack.length - 1];
-          var result = null;
-
-          this.stack.splice(this.stack.length - 1, 1);
-          this.stack.splice(this.stack.length - 1, 1);
-
-          switch (char) {
-            case "+":
-              result = num1 + num2;
-              console.log("Adding " + num1 + "+" + num2);
-              break;
-
-            case "-":
-              result = num1 - num2;
-              console.log("Subtracting " + num1 + "-" + num2);
-              break;
-
-            case "x": case "*":
-              result = num1 * num2;
-              console.log("Multiplying " + num1 + "*" + num2);
-              break;
-
-            case "/":
-              result = num1 / num2;
-              console.log("Dividing " + num1 + "/" + num2);
-              break;
-          }
-
-
-          this.stack.push(result);
-          console.log("Pushing the result of " + result + " on to the stack", this.stack);
-        }
-      }
-      if (this.stack.length != 1 || isNaN(this.stack[0])) {
-        this.stack = [];
-        this.stack.push("Invalid formula");
-        this.valid = false;
-      } else {
-        this.valid = true;
-      }*/
-    } } });
+}}});
